e836403db529e444ff156518563390795ebf8d46
nguyenductien.qnm <ndtien317@gmail.com>
2026-05-09 16:59:46 +0700

    refactor: add more permission

--- trello-api/src/constant/workspacePermission.constant.js
+++ trello-api/src/constant/workspacePermission.constant.js
@@ -12,5 +12,8 @@ export const WORKSPACE_PERMISSIONS = {
   ROLE_DELETE: 'workspace.role.delete', // done
 
   BOARD_CREATE: 'workspace.board.create', // done
-  BOARD_DELETE: 'workspace.board.delete'
+  BOARD_DELETE: 'workspace.board.delete',
+
+  WORKSPACE_UPGRADE: 'workspace.upgrade',
+  WORKSPACE_DOWNGRADE: 'workspace.downgrade'
 }
